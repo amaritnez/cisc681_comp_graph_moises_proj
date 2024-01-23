b44ffed8d0b1fd57516acf347294289714f77dd8
amaritnez <moisesmartinez61898@gmail.com>
2024-01-22 23:37:25 -0500

    assignment 1 pt 2

--- js/assignment1_2.js
+++ js/assignment1_2.js
@@ -25,77 +25,85 @@ function createScene() {
         // Default to 15 radius
         polygonRadiusSize = 15;
     }
-    // Set inner color
-    var innerColorString = myUrl2.searchParams.get('innerColor');
-    if (innerColorString == null) {
-        // Default to red
-        innerColorString = 'red';
+    // Set polygon length
+    var polygonLengthSize = myUrl2.searchParams.get('len');
+    if (polygonLengthSize == null) {
+        // Default to 20 length
+        polygonLengthSize = 15;
     }
-    // Set outer color
-    var outerColorString = myUrl2.searchParams.get('outerColor');
-    if (outerColorString == null) {
-        // Default to blue
-        outerColorString = 'blue';
-    }
-    // Setup colors
-    let innerColor = new THREE.Color(innerColorString);
-    let outerColor = new THREE.Color(outerColorString);
     // Actually make the polygon
-    let polygon = regularPolygonMesh(polygonSideCount, polygonRadiusSize, innerColor, outerColor);
+    let polygon = createCylinder(polygonSideCount, polygonRadiusSize, polygonLengthSize);
     // Add the polygon to the scene
     scene.add(polygon);
 }
 
 // Creates a regular polygon with N sides and rad radius, and has a color that gradually changes from innerColor to outerColor
-// n - number of sides the polygon will be; must be greater than 3, or else will throw an exception
-// rad - radius of the polygon. must be positive, or else will throw an exception
-// innerColor - defines the inner color of the polygon; colors are defined in the three.js library
-// outerColor - defines the outer color of the polygon; colors are defined in the three.js library
-function regularPolygonMesh(n, rad, innerColor, outerColor)
-{
+// n - number of sides the polygon will be
+// rad - radius of the polygon
+// len - height of the cylinder. It will be len tall, with it centered on the origin such that it's center lies on the y-plane and aligns with the y-axis
+function createCylinder(n, rad, len) {
+    // Setup geometry object
     let geom = new THREE.Geometry();
+    // Get height for bottom/top based on len value 
+    let height = len / 2;
 
-    // Setup array of vertices for use with the polygon, it will have n + 1 number of vertices
+    // Setup array of vertices for use with the cylinder, it will have 2n number of vertices
     let vertexArray = [];
-    // Now iterate for each vertex; save the last vertex for a special case, so only iterate for n
-    for (let i=0; i<n; i++) {
-        // Get angle of this cord, based on current vertex index
-        let angle = parseFloat((360/n) * i);
-        // Assume first vertex has x = 0
-        let xCord = rad * Math.sin(Math.PI * 2 * angle / 360.0);
-        let yCord = rad * Math.cos(Math.PI * 2 * angle / 360.0);
-        // Setup our vertext; assume all vertices are in same z-plane
-        vertexArray.push(new THREE.Vector3(xCord, yCord, 0));
-        // Add them to the array of verticies in the geom object
-        geom.vertices.push(vertexArray[i]);
-    }
-    // the n-th index vertex is a special vertex that represents the center of the polygon, create it now
-    let centerVertex = new THREE.Vector3(0, 0, 0);
-    geom.vertices.push(centerVertex);
-    // Now the fun part, create a set of triangles based on our vertices to form the polygon
-    // Each triangle uses vertex indicies j,j+1, and n to make itself
-    for (let j=0; j<n; j++) {
-        var currFace;
-        // Overflow check; at max index, we use index 0 instead of j+1 to avoid overflowing the array
-        if (j == n -1) {
-            currFace = new THREE.Face3(j, 0, n);
-            geom.faces.push(currFace);
-        } else {
-            currFace = new THREE.Face3(j, j+1, n);
-            geom.faces.push(currFace);
+    // The first iteration is for all the verticies in the same y-axis; do all bottom vertices, then all top verticies
+    for (let i = 0; i < 2; i++) {
+        // Now iterate for each vertex within that y plane to make the n-sided polygon
+        for (let j = 0; j < n; j++) {
+            // Get angle of this cord, based on current vertex index
+            let angle = parseFloat((360 / n) * j);
+            // Use the power of MATH to get the x/z cords (calculate different points along a circle)
+            let xCord = rad * Math.sin(Math.PI * 2 * angle / 360.0);
+            let zCord = rad * Math.cos(Math.PI * 2 * angle / 360.0);
+            // Setup our vertex using the calculated x/z coordinates
+            // The y-cord is based on whether this is the top or bottom plane
+            vertexArray.push(new THREE.Vector3(xCord, i == 0 ? height * -1 : height, zCord));
+            // Add them to the array of verticies in the geom object
+            geom.vertices.push(vertexArray[(i*n)+j]);
         }
-        // Now, setup the color for this triangle following the pattern of innerColor gradiating to outerColor
-        // Convieniently, the third vertex of all faces represents the inner vertex, so use vertex coloring
-        // where first two vertices use outerColor, and third vertex uses innerColor
-        currFace.vertexColors.push(outerColor, outerColor, innerColor);
     }
-    // Setup material + mesh here, be sure to include vertext coloring to get interporlating effect
-    let mat = new THREE.MeshBasicMaterial({vertexColors: THREE.VertexColors, side: THREE.DoubleSide});
+
+    // Now setup the faces for the cylinder; lets start with the bases //
+    // Iterate through bottom-top again
+    for (let k = 0; k < 2; k++) {
+        // For each base, we iterate through n-2 times making triangles
+        for (let l = 1; l < n-1; l++) {
+            // Use the first vertex of the base, the current iterator index, and current iterator index + 1
+            let face = new THREE.Face3(k * n, (k*n) + l, (k*n) + (l+1));
+            geom.faces.push(face);
+        }
+    }
+
+    // Finally, connect the two bases by making a face for each side //
+    // We will have N number of sides, so iterate that number of times
+    for (let p = 0; p < n; p++) {
+        // Each side face is made of two triangles, so double iterate
+        for (let m = 0; m < 1; m++) {
+            // Overflow check; at max index, we use index n instead of n+p+1 to avoid overflowing (we loop back to first index of the base)
+            if (p == n - 1) {
+                let overflowFace = new THREE.Face3(p, n + p, n);
+                geom.faces.push(overflowFace);
+            } else {
+                // Connect one bottom vertex with two top verticies (one of same index, one of index + 1)
+                let face1 = new THREE.Face3(p, n + p, n + p + 1);
+                geom.faces.push(face1);
+                // Connect two bottom verticies with one top vertex (top vertext is always p+1)
+                // Also convieniently, when this overflows it helps fill out the last triangle that isn't covered by the other if-block
+                let face2 = new THREE.Face3(p, p + 1, n + p + 1);
+                geom.faces.push(face2);
+            }
+        }
+    }
+
+    // Set face normals and mesh stuff, then return final shape
+    geom.computeFaceNormals();
+    let mat = new THREE.MeshBasicMaterial({color: 0xFF00FF, side: THREE.DoubleSide, wireframe: true});
     let mesh = new THREE.Mesh(geom, mat);
     return mesh;
-
 }
-
 
 function animate() {
 	window.requestAnimationFrame(animate);
